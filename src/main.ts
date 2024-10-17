18d5ab527e8032a301145462df41e870177442a3
Richard Neves <ricknevesbc@icloud.com>
2024-10-17 18:49:15 -0300

    fix: adjust error when serialize a BigInt

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { env } from './env'
 
+// Config for BigInt to fixed JSON.stringify() doesn't know how to serialize a BigInt, this is error in JSON.stringify(), possible error in nestjs
+// eslint-disable-next-line no-extend-native
+BigInt.prototype.toJSON = function () {
+  return this.toString()
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
