6377ac9f4ac49fcd1cda4c5f0e2c3c304bda4116
Richard Neves <ricknevesbc@icloud.com>
2024-10-18 22:17:27 -0300

    REMOVE CONFIGS VERCEL

--- src/common/tests/lib/prisma.ts
+++ src/common/tests/lib/prisma.ts
@@ -1,11 +1,6 @@
 import { PrismaService } from 'nestjs-prisma'
-import { env } from '~/env'
 
-export const prisma = new PrismaService({
-  prismaOptions: {
-    datasourceUrl: env.POSTGRES_PRISMA_URL,
-  },
-})
+export const prisma = new PrismaService()
 
 export const clearDatabase = async () => {
   await prisma.$transaction([
